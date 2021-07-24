475f577d1c03ba52b6f8563b986b6e256eb00143
Alessandra Ulinick <aleeulinick@gmail.com>
2021-07-24 17:15:01 -0300

    last

--- defaultdict.py
+++ defaultdict.py
@@ -1,2 +1,9 @@
 dicionario = {'curso': 'Programacao em python'}
 print(dicionario['curso'])
+
+#Fazendo import
+from collections import defaultdict
+
+dicionario = defaultdict(lambda: 0)
+dicionario['curso'] = 'Programacao em python'
+print(dicionario)
---
+++ exercicio2secao7.py
@@ -0,0 +1,2 @@
+for n in list(range (10)):
+    
